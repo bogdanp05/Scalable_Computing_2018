a17a306374c55609e347e7d67514dede51ac67de
Bogdan Petre <bogdan.petre0510@gmail.com>
2018-02-25 15:48:07 +0100

    fixed txt to csv conversion script

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # 2018_group_6_s3480941_s3346935_s3421902
 
-Get the dataset from https://labrosa.ee.columbia.edu/millionsong/tasteprofile.
-It will be a 1.8GB textfile.
-Use txt_to_csv.py to convert it to csv.
-Add the csv file in Project/src/main/resources/csv.
+- Get the dataset from https://labrosa.ee.columbia.edu/millionsong/tasteprofile. It will be a 1.8GB textfile.
+- Use txt_to_csv.py to convert it to csv.
+- Add the csv file in Project/src/main/resources/csv.
--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,10 +1,18 @@
 import csv
 
 schema = ['user','song','count']
-with open('train_triplets.txt', 'r') as in_file:
-    stripped = (line.strip() for line in in_file)
-    lines = (line.split(",") for line in stripped if line)
-    with open('train_triplets.csv', 'w') as out_file:
-        writer = csv.writer(out_file)
-        writer.writerow([name for name in schema])
-        writer.writerows(lines)
+# with open('mao.txt', 'r') as in_file:
+#     stripped = (line.strip() for line in in_file)
+#     lines = (line.split(",") for line in stripped if line)
+#     with open('mao.csv', 'w') as out_file:
+#         writer = csv.writer(out_file)
+#         writer.writerow([name for name in schema])
+#         writer.writerows(lines)
+
+txt_file = "train_triplets.txt"
+csv_file = "train_triplets.csv"
+
+in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+out_csv = csv.writer(open(csv_file, 'w'))
+out_csv.writerow([name for name in schema])
+out_csv.writerows(in_txt)
