c6786618a2721f963e58703b8dbd442331d11f97
Yuki_Kan <yukikan22@gmail.com>
2018-02-25 23:54:45 +0100

    insert dataset to mongodb in bulks

--- csv_to_mongo.py
+++ csv_to_mongo.py
@@ -4,19 +4,31 @@ import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
 #CSV to JSON Conversion
-csvfile = open('./Project/src/main/resources/csv/mao2000.csv', 'r')
+csvfile = open('./Project/src/main/resources/csv/train_triplets.csv', 'r')
+# csvfile = open('./Project/src/main/resources/csv/mao2000.csv', 'r')
 reader = csv.DictReader(csvfile)
 mongo_client = MongoClient()
-# db = mongo_client.music_data
-db = mongo_client.music_data2
+db = mongo_client.music_data
+# db = mongo_client.music_data2
 
-db.triplets2.drop()
+db.triplets.drop()
 
-header = [ "user", "song", "count"]
+header = ["user", "song", "count"]
 
+# insert to mongodb in bulk
+counter = 0
+bulk_number = 500
+bulk = []
 for each in reader:
     row = {}
     for field in header:
         row[field] = each[field]
 
-    db.triplets2.insert(row)
+    # db.triplets.insert(row)
+    bulk.append(row)
+    counter += 1
+
+    if counter % bulk_number == 0:
+        db.triplets.insert_many(bulk)
+        bulk = []
+        print(counter)
