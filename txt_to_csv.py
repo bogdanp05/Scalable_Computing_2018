6f92da0b431aa345eb0c13f07d76f777c94b9e8a
Bogdan Petre <bogdan.petre0510@gmail.com>
2018-03-01 10:39:34 +0100

    Updated architecture

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -7,21 +7,21 @@ schema = ['user','song','count']
 # csv_file = "/Users/Yukiii/Documents/9_Scalable_computing/project/Project/src/main/resources/csv/train_triplets.csv"
 
 
-# Create a csv with the first 2k rows
-# txt_file = "train_triplets.txt"
-# csv_file = "mao2.csv"
-# in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
-# out_csv = csv.writer(open(csv_file, 'w'))
-# out_csv.writerow([name for name in schema])
-# for i in range(0,2000):
-#     out_csv.writerow(next(in_txt))
+# Create a csv with the first 20k rows
+txt_file = "/home/bogdan/school_tmp/sc/train_triplets.txt"
+csv_file = "./Project/src/main/resources/csv/mao20k.csv"
+in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+out_csv = csv.writer(open(csv_file, 'w'))
+out_csv.writerow([name for name in schema])
+for i in range(0,20000):
+    out_csv.writerow(next(in_txt))
 
 
 # Create full csv
 
-txt_file = "train_triplets.txt"
-csv_file = "train_triplets.csv"
-in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
-out_csv = csv.writer(open(csv_file, 'w'))
-out_csv.writerow([name for name in schema])
-out_csv.writerows(in_txt)
+# txt_file = "train_triplets.txt"
+# csv_file = "train_triplets.csv"
+# in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+# out_csv = csv.writer(open(csv_file, 'w'))
+# out_csv.writerow([name for name in schema])
+# out_csv.writerows(in_txt)
